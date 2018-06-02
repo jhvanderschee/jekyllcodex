3f7eb32bac2f4bf39b06db2a19bb5592aaa4be53
Joost van der Schee <jhvanderschee@gmail.com>
2018-06-02 23:59:16 +0200

    update

---
+++ _includes/chatbutton.js
@@ -0,0 +1,1 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="08f726ab-0303-4a41-9563-b9a32526b8ea";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
--- _includes/ga.js
+++ _includes/ga.js
@@ -1,4 +1,3 @@
-<script>
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-70348270-1']);
   _gaq.push(['_gat._forceSSL']);
@@ -10,4 +9,3 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-</script>
